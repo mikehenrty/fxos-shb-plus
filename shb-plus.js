496090228b1a3232f31546b579cfb56dcd496542
Michael Henretty <michael.henretty@gmail.com>
2015-06-28 17:11:09 -0700

    Make Homebar Plus work after rebooting

--- shb-plus.js
+++ shb-plus.js
@@ -1,63 +1,82 @@
 (function() {
-  var buttons = document.getElementById('software-buttons');
-
-  // Clean up any old installs.
-  var old = document.getElementById('show-windows-button');
-  if (old) {
-    old.remove();
-  }
-  old = document.getElementById('kill-window-button');
-  if (old) {
-    old.remove();
-  }
-
-  var showWindows = document.createElement('button');
-  showWindows.id = 'show-windows-button';
-  showWindows.textContent = '▢';
-  showWindows.style.position = 'absolute';
-  showWindows.style.right = '1rem';
-  showWindows.style.top = 0;
-  showWindows.style.height = '5rem';
-  showWindows.style.width = '7rem';
-  showWindows.style.backgroundColor = 'red';
-  showWindows.style.zIndex = 99999999;
-  showWindows.style.pointerEvents = 'all';
-  showWindows.style.background = 'transparent';
-  showWindows.style.border = 'none';
-  showWindows.style.color = 'white';
-  showWindows.style.fontSize = '3rem';
-  showWindows.style.fontWeight = 'bold';
-  showWindows.addEventListener('touchstart', function() {
-    if (window.wrappedJSObject.appWindowManager.taskManager.isShown()) {
-      window.wrappedJSObject.appWindowManager.taskManager.exitToApp();
-    } else {
-      window.dispatchEvent(new CustomEvent('taskmanagershow'));
+  function init() {
+    var buttons = document.getElementById('software-buttons');
+    if (!buttons) {
+      console.log('michael, no homebar found?');
+      return;
     }
-  }, true);
-  buttons.appendChild(showWindows);
 
-  var kill = document.createElement('button');
-  kill.id = 'kill-window-button';
-  kill.textContent = '▽';
-  kill.style.position = 'absolute';
-  kill.style.left = '1rem';
-  kill.style.top = 0;
-  kill.style.height = '5rem';
-  kill.style.width = '7rem';
-  kill.style.backgroundColor = 'yellow';
-  kill.style.zIndex = 99999999;
-  kill.style.pointerEvents = 'all';
-  kill.style.background = 'transparent';
-  kill.style.border = 'none';
-  kill.style.color = 'white';
-  kill.style.fontSize = '3rem';
-  kill.style.fontWeight = 'bold';
-  kill.addEventListener('touchstart', function() {
-    var oldApp = window.wrappedJSObject.StackManager.getCurrent();
-    window.wrappedJSObject.SheetsTransition.begin('ltr');
-    window.wrappedJSObject.SheetsTransition.snapBack(1);
-    window.wrappedJSObject.StackManager.goPrev();
-    oldApp.kill();
-  }, true);
-  buttons.appendChild(kill);
+    // Clean up any old installs.
+    var old = document.getElementById('show-windows-button');
+    if (old) {
+      old.remove();
+    }
+    old = document.getElementById('kill-window-button');
+    if (old) {
+      old.remove();
+    }
+
+    var showWindows = document.createElement('button');
+    showWindows.id = 'show-windows-button';
+    showWindows.textContent = '▢';
+    showWindows.style.position = 'absolute';
+    showWindows.style.right = '1rem';
+    showWindows.style.top = 0;
+    showWindows.style.height = '5rem';
+    showWindows.style.width = '7rem';
+    showWindows.style.backgroundColor = 'red';
+    showWindows.style.zIndex = 99999999;
+    showWindows.style.pointerEvents = 'all';
+    showWindows.style.background = 'transparent';
+    showWindows.style.border = 'none';
+    showWindows.style.color = 'white';
+    showWindows.style.fontSize = '3rem';
+    showWindows.style.fontWeight = 'bold';
+    showWindows.addEventListener('touchstart', function() {
+      if (window.wrappedJSObject.appWindowManager.taskManager.isShown()) {
+        window.wrappedJSObject.appWindowManager.taskManager.exitToApp();
+      } else {
+        window.dispatchEvent(new CustomEvent('taskmanagershow'));
+      }
+    }, true);
+    buttons.appendChild(showWindows);
+
+    var kill = document.createElement('button');
+    kill.id = 'kill-window-button';
+    kill.textContent = '▽';
+    kill.style.position = 'absolute';
+    kill.style.left = '1rem';
+    kill.style.top = 0;
+    kill.style.height = '5rem';
+    kill.style.width = '7rem';
+    kill.style.backgroundColor = 'yellow';
+    kill.style.zIndex = 99999999;
+    kill.style.pointerEvents = 'all';
+    kill.style.background = 'transparent';
+    kill.style.border = 'none';
+    kill.style.color = 'white';
+    kill.style.fontSize = '3rem';
+    kill.style.fontWeight = 'bold';
+    kill.addEventListener('touchstart', function() {
+      var oldApp = window.wrappedJSObject.StackManager.getCurrent();
+      window.wrappedJSObject.SheetsTransition.begin('ltr');
+      window.wrappedJSObject.SheetsTransition.snapBack(1);
+      window.wrappedJSObject.StackManager.goPrev();
+      oldApp.kill();
+    }, true);
+    buttons.appendChild(kill);
+  }
+
+  // Make sure we have the homebar element before booting.
+  if (document.getElementById('software-buttons')) {
+    init();
+  } else {
+    window.addEventListener('mozContentEvent', function readyListener(e) {
+      if (e.detail.type === 'system-message-listener-ready') {
+        window.removeEventListener('mozContentEvent', readyListener);
+        init();
+      }
+    });
+  }
+
 }());
